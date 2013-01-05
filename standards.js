8071f82341eb4153e6b92e6929de33183f89a586
Trevor Ewen <tewen@neosavvy.com>
2013-01-05 16:46:59 -0500

    Added the empty standards file, so far not too many standards to force for angular.

---
+++ standards.js
@@ -0,0 +1,3 @@
+/* This js file is only intended as a starting point from the preferred code style in Neosavvy, Inc. application and library code */
+
+/* For additonal javascript style guides, check out the Neosavvy javascript-core project */
