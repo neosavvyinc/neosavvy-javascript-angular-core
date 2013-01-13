5533b906610a633d876095a147b3de5b89e9a81f
Trevor Ewen <tewen@neosavvy.com>
2013-01-12 22:32:55 -0500

    Added some libraries that were taken over from filmbot. Much more left to do with this.

---
+++ src/main/resources/directives/controls/tooltip-directive.js
@@ -0,0 +1,30 @@
+'use strict'
+
+Neosavvy.Directives
+    .directive('tooltip', function () {
+        return {
+            compile:function (tElement, tAttrs, transclude) {
+                var tooltipClazz = tAttrs.tooltip;
+                $(tElement[0]).append('<div style="display: none;" class="tooltip ' + tooltipClazz + '">&nbsp;</div>');
+                return function (scope, iElement, iAttrs) {
+                    //Adds Mouseover Handler to directive
+                    iElement.bind('mouseover', function (e) {
+                        scope.$apply(function () {
+                            var myTooltip = iElement[0].getElementsByClassName(tooltipClazz)[0];
+                            $(myTooltip).css('display', 'block');
+                            $(myTooltip).removeClass('animated fadeOut')
+                            $(myTooltip).addClass('animated fadeIn');
+                        });
+                    });
+                    iElement.bind('mouseleave', function (e) {
+                        scope.$apply(function () {
+                            var myTooltip = iElement[0].getElementsByClassName(tooltipClazz)[0];
+                            $(myTooltip).removeClass('animated fadeIn')
+                            $(myTooltip).addClass('animated fadeOut');
+                            $(myTooltip).css('display', 'none');
+                        });
+                    })
+                };
+            }
+        }
+    });
