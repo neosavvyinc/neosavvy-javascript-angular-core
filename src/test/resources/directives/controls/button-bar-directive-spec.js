da83d26d191415a8a42f6774a306b513a49cf6b4
Trevor Ewen <tewen@neosavvy.com>
2013-01-12 22:42:56 -0500

    Added all the testing project support. Need to add bootstrap still.

---
+++ src/test/resources/directives/controls/button-bar-directive-spec.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var body = $('body');
+
+var simpleHtml = '';
+
+describe("Button bar directive", function () {
+    var scope, $compile;
+
+    beforeEach(module('moe.directives'));
+
+    beforeEach(inject(function ($rootScope, $compile) {
+        scope = $rootScope.$new();
+        $compile = $compile;
+
+        //Define scope variables below, ex. scope.name = "Tom";
+    }));
+
+    afterEach(function () {
+        body.empty();
+    });
+
+    it('should compile the directive', function () {
+        expect(body.find('div').length).toBe(0);
+
+        var mock = $compile(simpleHtml)(scope);
+
+        expect(mock).not.toBeNull();
+    });
+});
