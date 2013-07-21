2c5dbe91a1294fdc18b4de285e807d83d20d8f1f
Trevor Ewen <tewen@neosavvy.com>
2013-07-21 14:53:18 -0400

    Removed some older garbage, starting with new things that are actually necessary.

--- src/main/resources/library/filters/number/to-ordinal.js
+++
@@ -1,8 +0,0 @@
-Neosavvy.Filters.filter('numberToOrdinal', function () {
-    return function (value) {
-        if (value || value == 0) {
-            return Neosavvy.Core.Utils.NumberUtils.toOrdinal(value);
-        }
-        return null;
-    }
-});
