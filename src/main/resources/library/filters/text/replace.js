2f5dd9aec3cfa1f697ec189f2bf5a6339fa1d118
Trevor Ewen <tewen@neosavvy.com>
2013-08-28 15:44:01 -0400

    Added the text replace.

---
+++ src/main/resources/library/filters/text/replace.js
@@ -0,0 +1,10 @@
+Neosavvy.AngularCore.Filters.filter("nsTextReplace", function() {
+    return function(val) {
+        if (!_.isEmpty(val) && arguments.length > 1) {
+            for (var i = 0; i < arguments.length; i++) {
+                val = val.replace(new RegExp(arguments[i], 'g'), "");
+            }
+        }
+        return val;
+    };
+});
