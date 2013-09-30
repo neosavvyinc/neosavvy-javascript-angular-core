c28e1295b7b96a5f11d4cfb2e2d46d81a9b9e3c4
Adam Parrish <aparrish@neosavvy.com>
2013-09-30 16:36:36 -0400

    Adding support to test the page filter.

---
+++ src/test/resources/library/filters/collection/page-spec.js
@@ -0,0 +1,46 @@
+describe("nsCollectionPage", function () {
+    var filter;
+
+    describe('page', function() {
+
+        beforeEach(function() {
+            module.apply(module, ['neosavvy.angularcore.filters'])
+
+            inject(function($filter){
+                filter = $filter('nsCollectionPage');
+            })
+        })
+
+        it('Should return an empty array when no data is passed', function() {
+            expect(filter()).toEqual([]);
+        })
+
+        it('Should return the first item of a 3 item array when count = 1', function() {
+
+            var items = [1,2,3];
+            var page1 = filter( items, 0, 1);
+            var page2 = filter( items, 1, 1);
+            var page3 = filter( items, 2, 1);
+            expect(page1[0]).toEqual(1);
+            expect(page2[0]).toEqual(2);
+            expect(page3[0]).toEqual(3);
+
+
+        });
+
+        it('Should return the first page of data in a 10 item set with 3 item pages', function() {
+
+            var items = [1,2,3,4,5,6,7,8,9,10];
+
+            var page1 = filter( items, 0, 3);
+
+            expect(page1).toBeDefined();
+            expect(page1.length).toEqual(3);
+            expect(page1[0]).toEqual(1);
+            expect(page1[1]).toEqual(2);
+            expect(page1[2]).toEqual(3);
+
+        })
+
+    })
+});
