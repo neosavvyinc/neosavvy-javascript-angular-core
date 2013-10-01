e14fb68917b4ecc2e239b2c55dd2e34cf2853d62
Trevor Ewen <tewen@neosavvy.com>
2013-10-01 12:45:13 -0400

    Documentation for all the collection filters.

--- src/main/resources/library/filters/collection/page.js
+++ src/main/resources/library/filters/collection/page.js
@@ -1,3 +1,25 @@
+/**
+ * @ngdoc filter
+ * @name neosavvy.angularcore.filters:nsCollectionPage
+ * @function
+ *
+ * @description
+ * Allows data pagination through the manipulation of a couple simple scope variables.
+ *
+ * Example of the inputs:
+ *   collection: [1, 3, 5, 8, 10, 11, 12, 27, 18, 28]
+ *   page: 1
+ *   count: 3
+ *
+ * Will return
+ *  [8, 10, 11]
+ *
+ *
+ * @example
+ * <pre>
+ * <label ng-repeat="item in someCollectionOnScope | nsCollectionPage : pageNumber : 10" ng-bind="item.name"></label>
+ * </pre>
+ */
 Neosavvy.AngularCore.Filters.filter('nsCollectionPage', function () {
     return function (collection, page, count) {
         if (collection && collection.length) {
