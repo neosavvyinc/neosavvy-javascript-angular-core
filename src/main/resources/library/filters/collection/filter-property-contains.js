92f62bf6801884c785322e93db34272b38aa8e3c
Trevor Ewen <tewen@neosavvy.com>
2013-10-01 11:55:48 -0400

    Documentation for nsCollectionFilterPropertyContains.

--- src/main/resources/library/filters/collection/filter-property-contains.js
+++ src/main/resources/library/filters/collection/filter-property-contains.js
@@ -1,3 +1,29 @@
+/**
+ * @ngdoc filter
+ * @name neosavvy.angularcore.filters:nsCollectionFilterPropertyContains
+ * @function
+ *
+ * @description
+ * This returns items in the collection that contain the string specified in the value.
+ *
+ * Example of the inputs:
+ *   collection: [{name:'Mr. Adam', value:1},{name:'Mr. Trevor', value:2},{name:'Dr. Chris',value:3}]
+ *   property: name
+ *   value: 'Dr.'
+ *
+ * Will return
+ *  [{name:'Dr. Chris',value:3}]
+ *
+ *
+ * @example
+ * <pre>
+ * <input ng-model={{someCollection | nsCollectionFilterPropertyContains:'propertyName':val}}/>
+ * </pre>
+ *
+ * <pre>
+ * <input ng-model={{someCollection | nsCollectionFilterPropertyContains:'deep.property.name':val}}/>
+ * </pre>
+ */
 Neosavvy.AngularCore.Filters.filter('nsCollectionFilterPropertyContains', function () {
     return function (collection, property, value) {
         if (collection && value) {
