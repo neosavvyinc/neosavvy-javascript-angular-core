59d37c2a93d3d1809a8ea5cdee536674d8afb79d
Adam Parrish <aparrish@neosavvy.com>
2013-10-01 10:42:28 -0400

    Asserting some basic performance.

--- src/test/resources/library/filters/text/replace-spec.js
+++ src/test/resources/library/filters/text/replace-spec.js
@@ -32,4 +32,22 @@ describe('nsTextReplace filter', function () {
         expect(response).toEqual('sandwiches');
     });
 
+    it('should perform really fast with 1000000 elements', function() {
+
+        var itemsToReplace = [];
+        for( var i = 0; i <= 1000000; i++) {
+            itemsToReplace[i] = 'porkchop sandwiches';
+        }
+
+        var startTime = new Date();
+        for( var j = 0; j <= 1000000; j++) {
+            itemsToReplace[j] = nsTextReplace(itemsToReplace[j], "porkchop ");
+        }
+        var endTime = new Date();
+
+        var difference = endTime - startTime;
+        expect(difference).toBeLessThan(3000);
+
+    })
+
 });
