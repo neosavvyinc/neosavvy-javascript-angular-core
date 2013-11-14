5d46eb61b5576bda5c4fddbcf6f842098698586d
Trevor Ewen <tewen@neosavvy.com>
2013-11-14 13:31:00 -0500

    Required if shown validator.

---
+++ src/main/resources/library/directives/validation/required-if-shown.js
@@ -0,0 +1,16 @@
+Neosavvy.AngularCore.Directives
+    .directive('nsRequiredIfShown',
+    function () {
+        return {
+            require: 'ngModel',
+            link: function (scope, element, attrs, ctrl) {
+                ctrl.$parsers.unshift(function (viewValue) {
+                    var valid = (!element.is(':visible') || !Neosavvy.Core.Utils.StringUtils.isBlank(viewValue));
+                    console.log("VISIBLE: " + element.is(':visible'));
+                    console.log("BLANK: " + Neosavvy.Core.Utils.StringUtils.isBlank(viewValue));
+                    ctrl.$setValidity('nsRequiredIfShown', valid);
+                    return valid ? viewValue : undefined;
+                })
+            }
+        }
+    });
