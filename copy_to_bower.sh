41a7561805aefd2e980ee30799810771cfe2a7c8
Trevor Ewen <tewen@neosavvy.com>
2013-12-11 15:45:20 -0500

    Version 0.1.2

--- copy_to_bower.sh
+++ copy_to_bower.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cp target/*.js ../bower-neosavvy-javascript-angular-core
 cp bower.json ../bower-neosavvy-javascript-angular-core
+sed -i.bak "s/neosavvy-javascript-angular-core-development/neosavvy-javascript-angular-core/g" ../bower-neosavvy-javascript-angular-core/bower.json
+rm ../bower-neosavvy-javascript-angular-core/bower.json.bak
