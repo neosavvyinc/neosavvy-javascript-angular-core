07942c74142efdc4191d2b5f3775c1c9484077d5
unicodesnowman <utfsnowman@gmail.com>
2014-03-04 18:26:22 -0500

    adding A for attribute, including tooltip option

--- src/main/resources/library/directives/containers/ns-modal.js
+++ src/main/resources/library/directives/containers/ns-modal.js
@@ -1,18 +1,32 @@
 Neosavvy.AngularCore.Directives.directive('nsModal', [
     'nsModal',
     function (nsModalService) {
+
+        function positionTooltip (e, element) {
+            var localX = 0.15 * e.currentTarget.clientWidth;
+            var localY = 0.65 * e.currentTarget.clientHeight;
+            var containerCoords = $(e.target).offset();
+            var xPos = localX + containerCoords.left;
+            var yPos = localY + containerCoords.top;
+            element.css("position", "absolute");
+            element.css("top", yPos);
+            element.css("left", xPos);
+            return element;
+        }
+
         return {
-            restrict: 'E',
+            restrict: 'EA',
             transclude: 'element',
             replace: true,
             scope: false,
             compile: function (tElem, tAttr, transclude) {
                 return function (scope, elem, attr) {
-                    var childScope = scope.$new();
+                    var childScope = scope.$new(),
+                        isTooltip = !!attr.tooltip;
 
                     if (typeof attr.open !== 'string') {
                         throw 'an open handler was not specified';
-                    }
+                    };
 
                     // close modal on route change
                     scope.$on('$routeChangeStart', function (e) {
@@ -23,7 +37,8 @@ Neosavvy.AngularCore.Directives.directive('nsModal', [
 
                     scope[attr.open] = function () {
                         transclude(childScope, function (clone) {
-                            nsModalService.open(childScope, clone, closeCallback);
+                            var element = isTooltip ? positionTooltip(e, clone) : clone;
+                            nsModalService.open(childScope, element, closeCallback);
                         });
                     };
 
