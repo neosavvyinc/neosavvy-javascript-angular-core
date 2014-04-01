188ca8866bcd32b5b92d7314fa53b46bcb642842
Adam Parrish <aparrish@neosavvy.com>
2014-04-01 17:24:23 -0400

    We need to be able to set an external CSRFToken header for jqRequest - this supports that functionality.

--- src/main/resources/library/services/service-extensions.js
+++ src/main/resources/library/services/service-extensions.js
@@ -59,6 +59,28 @@ Neosavvy.AngularCore.Services.factory('nsServiceExtensions',
                 }
             }
 
+            /**
+             * Below to functions from: https://docs.djangoproject.com/en/dev/ref/contrib/csrf/
+             */
+            function csrfSafeMethod(method) {
+                // these HTTP methods do not require CSRF protection
+                return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+            }
+
+            function sameOrigin(url) {
+                // test that a given url is a same-origin URL
+                // url could be relative or scheme relative or absolute
+                var host = document.location.host; // host + port
+                var protocol = document.location.protocol;
+                var sr_origin = '//' + host;
+                var origin = protocol + sr_origin;
+                // Allow absolute or scheme relative URLs to same origin
+                return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+                    (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+                    // or any other URL that isn't scheme relative or absolute i.e relative.
+                    !(/^(\/\/|http:|https:).*/.test(url));
+            }
+
             return {
                 /**
                  * @ngdoc method
@@ -206,6 +228,18 @@ Neosavvy.AngularCore.Services.factory('nsServiceExtensions',
                         }
                         if (params.ajax) {
                             request = _.merge(request, params.ajax);
+                        }
+                        if (params.csrfToken) {
+                            $.ajaxSetup({
+                                beforeSend: function(xhr, settings) {
+                                    if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
+                                        // Send the token to same-origin, relative URLs only.
+                                        // Send the token only if the method warrants CSRF protection
+                                        // Using the CSRFToken value acquired earlier
+                                        xhr.setRequestHeader("X-CSRFToken", params.csrfToken);
+                                    }
+                                }
+                            });
                         }
                         var jqXhr = $.ajax(request);
                         jqXhr.done(function (data, textStatus) {
